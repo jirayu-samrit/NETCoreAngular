ff855fda85cdb1c03c2941fb9aa51018a53234b2
SlowMo <jirayu.samrit@gmail.com>
2023-12-05 16:24:42 +0700

    End Section 2

---
+++ NETCoreAngular.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETCoreAngular.API.Entities;
+
+namespace NETCoreAngular.API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ NETCoreAngular.API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETCoreAngular.API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+}
