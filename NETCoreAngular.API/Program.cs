066291a7001a38961b41a1461d35a312ff5053bf
SlowMo <jirayu.samrit@gmail.com>
2023-12-06 16:30:02 +0700

    End of Section 3

--- NETCoreAngular.API/Program.cs
+++ NETCoreAngular.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -16,6 +17,14 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder =>
+{
+    builder
+    .WithOrigins("https://localhost:4200")
+    .AllowAnyHeader()
+    .AllowAnyMethod();
+});
 
 app.MapControllers();
 
