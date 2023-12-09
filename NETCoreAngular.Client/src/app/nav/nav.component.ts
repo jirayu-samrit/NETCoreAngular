dfa2a078d86388a53e8cfa41d1af793f7fe75481
SlowMo <jirayu.samrit@gmail.com>
2023-12-09 14:53:38 +0700

    End of section 6

--- NETCoreAngular.Client/src/app/nav/nav.component.ts
+++ NETCoreAngular.Client/src/app/nav/nav.component.ts
@@ -3,6 +3,8 @@ import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
 import { Observable } from 'rxjs/internal/Observable';
 import { of } from 'rxjs/internal/observable/of';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-nav',
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit
 {
 
     model: any = {}
-    constructor(public accountService: AccountService) { }
+    constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
     ngOnInit(): void
     {
@@ -23,14 +25,8 @@ export class NavComponent implements OnInit
     login()
     {
         this.accountService.login(this.model).subscribe({
-            next: res =>
-            {
-                console.log(res);
-            },
-            error: err =>
-            {
-                console.log(err);
-            }
+            next: _ => this.router.navigateByUrl("/members"),
+            error: err => this.toastr.error(err.error),
         }
         );
     }
@@ -38,5 +34,6 @@ export class NavComponent implements OnInit
     logout()
     {
         this.accountService.logout();
+        this.router.navigateByUrl("/");
     }
 }
